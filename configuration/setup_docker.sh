2005e90b27e7473177f7dfd1dd1d21fc48237fb7
Panagiotis Giannakoulias <panosg@harver.com>
2019-04-28 01:59:11 +0200

    improves automation with terraform

---
+++ configuration/setup_docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+git clone https://github.com/panosg87/twitter-trends.git
+
+cd twitter-trends
+git checkout improvements
+
+export \
+  TWITTER_CONSUMER_KEY=$1 \
+  TWITTER_CONSUMER_SECRET=$2 \
+  TWITTER_OAUTH_TOKEN=$3 \
+  TWITTER_OAUTH_TOKEN_SECRET=$4
+
+sudo -E docker-compose up
--- start_airflow.sh
+++ start_airflow.sh
@@ -3,15 +3,11 @@
 airflow initdb
 
 AIRFLOW_HOME=$1
-TWIITER_CONSUMER_KEY=$2
-TWITTER_CONSUMER_SECRET=$3
-TWITTER_OAUTH_TOKEN=$4
-TWITTER_OAUTH_TOKEN_SECRET=$5
 
-airflow variables -s TWITTER_CONSUMER_KEY $TWIITER_CONSUMER_KEY
-airflow variables -s TWITTER_CONSUMER_SECRET $TWITTER_CONSUMER_SECRET
-airflow variables -s TWITTER_OAUTH_TOKEN $TWITTER_OAUTH_TOKEN
-airflow variables -s TWITTER_OAUTH_TOKEN_SECRET $TWITTER_OAUTH_TOKEN_SECRET
+airflow variables -s TWITTER_CONSUMER_KEY $2
+airflow variables -s TWITTER_CONSUMER_SECRET $3
+airflow variables -s TWITTER_OAUTH_TOKEN $4
+airflow variables -s TWITTER_OAUTH_TOKEN_SECRET $5
 
 cp $AIRFLOW_HOME/twitter-trends/airflow_pipelines/twitter_trends_pipeline.py $AIRFLOW_HOME/dags/
 cp -r $AIRFLOW_HOME/twitter-trends/airflow_pipelines/twitter_trends/ $AIRFLOW_HOME/dags/
